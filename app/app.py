17bfc41693d81faac890a73f1fd0883bfcff81f5
Davi Santos <22971355+daviribeirods@users.noreply.github.com>
2025-03-22 14:23:23 -0300

    Creating multipage app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,22 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
+import dash
+from dash import Dash, html, dcc
 
-app = Dash()
+app = Dash(__name__, use_pages=True)
 
-app.layout = [
-    html.H1(children='Rent Analytics App', style={'textAlign':'center'}),
-]
+app.layout = html.Div(
+    [
+        # main app framework
+        html.Div("Brasilia Rent Analytics App", style={'fontSize':50, 'textAlign':'center'}),
+        html.Div([
+            dcc.Link(page['name']+"  |  ", href=page['path'])
+            for page in dash.page_registry.values()
+        ]),
+        html.Hr(),
+
+        # content of each page
+        dash.page_container
+    ]
+)
 
 if __name__ == '__main__':
     app.run(debug=True)
