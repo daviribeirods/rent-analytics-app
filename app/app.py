247a8eaa47f4733c2a2a18d6ba2d3615c2a24f15
Davi Santos <22971355+daviribeirods@users.noreply.github.com>
2025-03-23 16:32:34 -0300

    Update: added components to home page

--- app/app.py
+++ app/app.py
@@ -2,31 +2,56 @@ import dash
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
-app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.COSMO]) # Themes I like: Zephyr, Pulse, Cosmo, Flatly, Sketchy, Journal
 
 app.layout = dbc.Container(
     [
-        # main app framework
+        # First row of the app
         dbc.Row(
-            [
-                html.Div(
-                    children = ['Brasilia Rent Analytics App'],
-                    style = {
-                        'fontSize': 50,
-                        'textAlign': 'center'
-                    }
+            children = [
+                # App title
+                dbc.Col(
+                    children = [
+                        dbc.Stack(
+                            children = [
+                                html.Div(
+                                    children = ['RentPrice Brasília.'],
+                                    style = {
+                                        'fontSize': 60,
+                                    },
+                                ),
+                                html.Div(
+                                    children = [' 2025'],
+                                    style = {
+                                        'fontSize': 58,
+                                        'color': 'red'
+                                    },
+                                )
+                            ],
+                            direction = 'horizontal',
+                            gap = 5
+                        )
+                    ]
+                ),
+
+                # App buttons links
+                dbc.Col(
+                    children = [
+                        dbc.Button('Home', color='primary', className='mt-4 mx-1', href='/'),
+                        dbc.Button('Analytics', color='primary', className='mt-4 mx-1', href='/analytics'),
+                        dbc.Button('About', color='primary', className='mt-4 mx-1', href='/about'),
+                    ],
+                    width=3
                 )
-            ]
+            ],
+            className = 'mt-4 mx-3' 
         ),
-        html.Div([
-            dcc.Link(page['name']+"  |  ", href=page['path'])
-            for page in dash.page_registry.values()
-        ]),
         html.Hr(),
 
         # content of each page
         dash.page_container
-    ]
+    ],
+    fluid=True
 )
 
 if __name__ == '__main__':
--- app/pages/home.py
+++ app/pages/home.py
@@ -1,10 +1,87 @@
 import dash
 from dash import html
 import dash_bootstrap_components as dbc
+from dash import dcc
 
 dash.register_page(__name__, path='/')
 
-layout = html.Div([
-    html.H1('This is our Home page'),
-    html.Div('This is our Home page content.')
-])
+# layout = html.Div([
+#     html.H1('This is our Home page'),
+#     html.Div('This is our Home page content.')
+# ])
+
+layout = dbc.Container(
+    [   
+        # Home page content
+        dbc.Row(
+            children = [
+                # Graph content
+                dbc.Col(
+                    children = [
+                        dcc.Graph()
+                    ],
+                    width=8
+                ),
+                # Menu to change graph content
+                dbc.Col(
+                    children = [
+                        html.H2(
+                            children = [
+                                'Explore the best rental properties in Brasília with RentPrice, powered by AI.'
+                            ],
+                            className='mt-2'
+                        ),
+                        html.P(
+                            children = [
+                                'Brasília is divided administratively into different regions. Select the region to see the houses and apartments by price.'
+                            ], 
+                            className='mt-4'
+                        ),
+                                                # Input to select type of property
+                        html.P(
+                            children = ['Select the type of the property'],
+                            style = {
+                                'font-weight': 'bold'
+                            },
+                            className = 'text-bold mt-3'
+                        ),
+                        dcc.Dropdown(
+                            options = [
+                                'All',
+                                'Apartment',
+                                'House'
+                            ],
+                            value = 'All',
+                            placeholder='Select the type of properties'
+                        ),
+                        # Input to select region
+                        html.P(
+                            children = ['Select the region in Brasília'],
+                            style = {
+                                'font-weight': 'bold'
+                            },
+                            className = 'text-bold mt-3'
+                        ),
+                        dcc.Dropdown(
+                            options = [
+                                'Region A',
+                                'Region B'
+                            ],
+                            placeholder='Select the region'
+                        ),
+                        dbc.Card(
+                            dbc.CardBody(
+                                children = [
+                                    html.H5('Select a region to see properties.'),
+                                ],
+                                className = 'text-center'
+                            ),
+                            className = 'mt-4'
+                        )
+                    ]
+                )
+            ]
+        )
+    ],
+    fluid=True
+)
