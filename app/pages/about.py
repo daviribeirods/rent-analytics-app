5556bf5e9f5e796ce42ee3ec7a05dcb23a4aa39d
Davi Santos <22971355+daviribeirods@users.noreply.github.com>
2025-03-24 23:44:57 -0300

    Add: Added some description to about page

--- app/pages/about.py
+++ app/pages/about.py
@@ -4,7 +4,41 @@ import dash_bootstrap_components as dbc
 
 dash.register_page(__name__)
 
-layout = html.Div([
-    html.H1('This is the about page'),
-    html.Div('This is the about page content')
-])
+layout = dbc.Container(
+    [
+        # Title of About page
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.H1('About the project')
+                    ]
+                )
+            ],
+            className = 'mx-2',
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.P(
+                            [
+                                'This project is written and developed by Davi Santos. Davi Santos is a data scientist, data analyst and Bachelor in Computer Science from UFT.'
+                                'This project intended to solver Davi\'s personal idea for searching a new apartament to live. Daily, he used to visit different websites'
+                                'search for a new rental property. And that\'s when he came up with this idea: gathering in one place information about rental properties'
+                                'and to some analysis.'
+                            ]
+                        )
+                    ]
+                ),
+                dbc.Col(
+                    [
+                        'This is another column'
+                    ]
+                )
+            ],
+            className = 'mx-2 mt-4'
+        )
+    ],
+    fluid=True    
+)
